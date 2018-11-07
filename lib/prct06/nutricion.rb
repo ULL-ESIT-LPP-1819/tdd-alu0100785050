3d6134defec69f290e69927d9b0c1a3d53e29a80
alu0100785050 <alu0100785050@ull.edu.es>
2018-11-07 09:47:47 +0000

    Test IR en porcion 100 gramos

--- lib/prct06/nutricion.rb
+++ lib/prct06/nutricion.rb
@@ -20,10 +20,15 @@ class Etiqueta
 		@vitaminas = vitaminas
 	end
 
-	def valorEnergetico
+	def valorEnergeticoKJ
 		kj = ((grasas.to_f * 37) + (saturadas.to_f * 37) + (mono.to_f * 37) + (poli.to_f * 37) + (hidratos.to_f * 17) + (azucares.to_f * 17) + (polial.to_f * 10) + (almidon.to_f * 17) + (fibra.to_f * 8) + (proteinas.to_f * 17) + (sal.to_f * 25)).round(2)
+		
+		kj.round(2)
+	end
+
+	def valorEnergeticoKcal
 		kcal = ((grasas.to_f * 9) + (saturadas.to_f * 9) + (mono.to_f * 9) + (poli.to_f * 9) + (hidratos.to_f * 4) + (azucares.to_f * 4) + (polial.to_f * 2.4) + (almidon.to_f * 4) + (fibra.to_f * 2) + (proteinas.to_f * 4) + (sal.to_f * 6))
 		
-		(kj/kcal).round(2)
+		kcal.round(2)
 	end
 end 
--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe Prct06 do
 			      "0.00","0.00","14.6","0.6","10.14")
   end
 
-  describe "Atributos bien inicializados" do
-  	it do 
+  describe "Initialize" do
+  	it "Atributos bien inicializados" do 
 	  expect(@et1.alimento).to eq("Bacon")
 	  expect(@et1.grasas).to eq("69.30")
 	  expect(@et1.saturadas).to eq("28.21")
@@ -24,10 +24,36 @@ RSpec.describe Prct06 do
   end
   
   describe "Valor energético" do
-	it "El valor energético debe estar definido en un método" do
-		expect(@et1.valorEnergetico).to eq(4.12)
+	it "El valor energético (KJ) debe estar definido en un método" do
+		expect(@et1.valorEnergeticoKJ).to eq(5137.95)
+	end
+	it "El valor energético (Kcal) debe estar definido en un método" do
+		expect(@et1.valorEnergeticoKcal).to eq(1247.75)
 	end
   end
 
+  describe "IR por 100 gr de producto" do
+	it "Método para el valor energético" do
+		expect(@et1.valorEnergeticoKJ).to eq(5137.95)
+	end
+	it "Método para porcentaje de grasa" do
+		expect(@et1.grasaIR).to eq(42.80)
+	end
+	it "Método para porcentaje de saturadas" do
+		expect(@et1.saturadasIR).to eq(12.23)
+	end
+	it "Método para porcentaje de hidratos" do
+		expect(@et1.hidratosIR).to eq(159.00)
+	end
+	it "Método para porcentaje de azúcares" do
+		expect(@et1.azucaresIR).to eq(55.04)
+	end
+	it "Método para porcentaje de proteinas" do
+		expect(@et1.proteinasIR).to eq(30.58)
+	end
+	it "Método para porcentaje de sal" do
+		expect(@et1.salIR).to eq(3.67)
+	end
+  end
 end
 
