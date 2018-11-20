85a30f546d31bc58d5fe8db992ec9459d30b7a4d
alu0100785050 <alu0100785050@ull.edu.es>
2018-11-20 11:07:41 +0000

    Pueba fallida initialize paciente

--- spec/prct08_spec.rb
+++ spec/prct08_spec.rb
@@ -5,7 +5,16 @@ RSpec.describe "Test herencia - Paciente" do
 	
 	before :each do
 		@paciente1 = Paciente.new(45,0,58,1.78,70,95)
-		@individuo1 = Individuo.new("Lucas","Perez",1,78)
+		@individuo1 = Individuo.new("Lucas","Perez",1,85)
+	end
+
+	it "Método initialize paciente" do
+		expect(@paciente1.edad).to eq(45)
+		expect(@paciente1.sexo).to eq(0)
+		expect(@paciente1.peso).to eq(58)
+		expect(@paciente1.talla).to eq(1.78)
+		expect(@paciente1.circun_cintu).to eq(70)
+		expect(@paciente1.circun_cadera).to eq(95)
 	end
 	
   end
