fc4f4a667ae32f4b4bfdf196311d0c56e0d2f54e
alu0100785050 <alu0100785050@ull.edu.es>
2018-12-19 11:43:20 +0000

    Añadido archivo array

---
+++ lib/prct06/array.rb
@@ -0,0 +1,17 @@
+class Array
+
+	def buclesfor
+	  swapped = true
+	    for i in 0 ...(self.length-1)
+	    swapped = false
+	      for j in 0 ... (self.length-i-1)
+	        if self[j] > self[j+1]
+	          self[j] , self[j+1] = self[j+1], self[j]
+		  swapped = true
+		end
+	      end
+	    break if swapped == false
+	    end
+	    return self
+	end
+end
